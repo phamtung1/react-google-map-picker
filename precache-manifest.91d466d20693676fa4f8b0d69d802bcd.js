0b8db1eb2f4a23e251a9288a68055dde24406d5d
Pham Tung <pham.tung1@gmail.com>
2020-05-14 13:59:54 +0700

    Updates

--- precache-manifest.91d466d20693676fa4f8b0d69d802bcd.js
+++ precache-manifest.91d466d20693676fa4f8b0d69d802bcd.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "40a962e08acbb0abe261fbed039d29f6",
+    "revision": "5f80976fa72e9f95c1156b5a1d84d07d",
     "url": "/react-google-map-picker/index.html"
   },
   {
-    "revision": "8ee260c6f370b0ebc7e2",
-    "url": "/react-google-map-picker/static/css/main.bd3804c8.chunk.css"
+    "revision": "b231203bab73c0cc2ef1",
+    "url": "/react-google-map-picker/static/css/main.432df639.chunk.css"
   },
   {
     "revision": "5bd1215b6563a0b082af",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-google-map-picker/static/js/2.90696476.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8ee260c6f370b0ebc7e2",
-    "url": "/react-google-map-picker/static/js/main.448a072c.chunk.js"
+    "revision": "b231203bab73c0cc2ef1",
+    "url": "/react-google-map-picker/static/js/main.d32e2b65.chunk.js"
   },
   {
     "revision": "818af22495eb83e347d2",
