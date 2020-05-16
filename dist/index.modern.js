dd113403fa61baaea8ebee998bd2dc36c19f1454
phamtung <pham.tung1@gmail.com>
2020-05-16 15:07:30 +0700

    Publish and Build example

--- dist/index.modern.js
+++ dist/index.modern.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 function loadScript(src, id) {
-  const script = document.createElement('script');
+  var script = document.createElement('script');
   script.setAttribute('async', '');
   script.setAttribute('id', id);
   script.src = src;
   document.querySelector('head').appendChild(script);
-  return new Promise(resolve => {
-    script.onload = () => {
+  return new Promise(function (resolve) {
+    script.onload = function () {
       resolve();
     };
   });
@@ -17,24 +17,24 @@ function isValidLocation(location) {
   return location && Math.abs(location.lat) <= 90 && Math.abs(location.lng) <= 180;
 }
 
-const GOOGLE_SCRIPT_URL = 'https://maps.googleapis.com/maps/api/js?libraries=places&key=';
-const MAP_VIEW_ID = 'google-map-view-' + Math.random().toString(36).substr(2, 9);
+var GOOGLE_SCRIPT_URL = 'https://maps.googleapis.com/maps/api/js?libraries=places&key=';
+var MAP_VIEW_ID = 'google-map-view-' + Math.random().toString(36).substr(2, 9);
 
-const MapPicker = ({
-  apiKey,
-  defaultLocation,
-  zoom: _zoom = 7,
-  onChangeLocation,
-  onChangeZoom,
-  style,
-  className
-}) => {
-  const map = React.useRef(null);
-  const marker = React.useRef(null);
+var MapPicker = function MapPicker(_ref) {
+  var apiKey = _ref.apiKey,
+      defaultLocation = _ref.defaultLocation,
+      _ref$zoom = _ref.zoom,
+      zoom = _ref$zoom === void 0 ? 7 : _ref$zoom,
+      onChangeLocation = _ref.onChangeLocation,
+      onChangeZoom = _ref.onChangeZoom,
+      style = _ref.style,
+      className = _ref.className;
+  var map = React.useRef(null);
+  var marker = React.useRef(null);
 
   function handleChangeLocation() {
     if (onChangeLocation) {
-      const currentLocation = marker.current.getPosition();
+      var currentLocation = marker.current.getPosition();
       onChangeLocation(currentLocation.lat(), currentLocation.lng());
     }
   }
@@ -44,14 +44,14 @@ const MapPicker = ({
   }
 
   function loadMap() {
-    const Google = window.google;
-    const validLocation = isValidLocation(defaultLocation) ? defaultLocation : {
+    var Google = window.google;
+    var validLocation = isValidLocation(defaultLocation) ? defaultLocation : {
       lat: 0,
       lng: 0
     };
     map.current = new Google.maps.Map(document.getElementById(MAP_VIEW_ID), {
       center: validLocation,
-      zoom: _zoom
+      zoom: zoom
     });
 
     if (!marker.current) {
@@ -66,28 +66,28 @@ const MapPicker = ({
     }
 
     map.current.addListener('click', function (event) {
-      const clickedLocation = event.latLng;
+      var clickedLocation = event.latLng;
       marker.current.setPosition(clickedLocation);
       handleChangeLocation();
     });
     map.current.addListener('zoom_changed', handleChangeZoom);
   }
 
-  React.useEffect(() => {
+  React.useEffect(function () {
     loadScript(GOOGLE_SCRIPT_URL + apiKey, 'google-maps').then(loadMap);
   }, []);
-  React.useEffect(() => {
+  React.useEffect(function () {
     if (marker.current) {
       map.current.setCenter(defaultLocation);
       marker.current.setPosition(defaultLocation);
     }
   }, [defaultLocation]);
-  React.useEffect(() => {
+  React.useEffect(function () {
     if (map.current) {
-      map.current.setZoom(_zoom);
+      map.current.setZoom(zoom);
     }
-  }, [_zoom]);
-  const componentStyle = Object.assign({
+  }, [zoom]);
+  var componentStyle = Object.assign({
     width: '100%',
     height: '600px'
   }, style || {});
